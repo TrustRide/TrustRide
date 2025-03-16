f3452bf576d8594b1eb0d3c75ea48b936b6794d6
sejun0205 <82365920+sejun0205@users.noreply.github.com>
2025-03-16 15:40:33 +0900

    main 메서드 추가
    2025-03-16

--- gearshift/src/main/java/com/fastcampus/gearshift/controller/PUserMainController.java
+++ gearshift/src/main/java/com/fastcampus/gearshift/controller/PUserMainController.java
@@ -1,4 +1,12 @@
 package com.fastcampus.gearshift.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class PUserMainController {
+    @GetMapping("/main")
+    public String main(){
+        return "main";
+    }
 }
