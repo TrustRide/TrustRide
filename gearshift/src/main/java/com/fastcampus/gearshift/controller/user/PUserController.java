f58b1b7a1b5ba65e100b9c3de30672d21fd54cbe
su-jin-shin <sujinshin1@gmail.com>
2025-03-22 23:46:48 +0900

    refactor: update PUserController.java

--- gearshift/src/main/java/com/fastcampus/gearshift/controller/user/PUserController.java
+++ gearshift/src/main/java/com/fastcampus/gearshift/controller/user/PUserController.java
@@ -12,9 +12,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 import java.util.List;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -23,12 +20,14 @@ import org.slf4j.LoggerFactory;
 @Controller
 public class PUserController {
     private static final Logger logger = LoggerFactory.getLogger(PUserController.class);
+
     @Autowired
     PCateService cateService;
 
     @Autowired
     PHolderService pHolderService;
-    @GetMapping("/main")
+
+    @GetMapping("/")
     public String index(){
         return "user/userIndex";
     }
@@ -37,37 +36,7 @@ public class PUserController {
     public String login2(){
         return "user/loginForm";
     }
-  
-    @GetMapping("/loginTest")
-    public String login(){
-        return "user/loginForm";
-    }
-
-
-    //로그인 테스트
-    @PostMapping("/loginTest")
-    public String login(HttpServletRequest request, HttpServletResponse response, Model model, String id, String pwd){
-
-        if(id.equals("user11@example.com")&&pwd.equals("pwd11")) {
-            model.addAttribute("id", id);
-            model.addAttribute("pwd", pwd);
-
-            request.getSession().setAttribute("userId", id);
-            return "redirect:/";
-        }
-        return "redirect:/";
-
-    }
-
-    //로그아웃 테스트
-    @GetMapping("/logout")
-    public String logout(HttpServletRequest request,HttpServletResponse response){
-        HttpSession session =request.getSession();
-        session.invalidate();
-        return "redirect:/";
-    }
-
-
+    
     //카테고리
     @RequestMapping(value = "/userList", method = RequestMethod.GET)
     public String getList(Model model) throws Exception {
@@ -100,8 +69,6 @@ public class PUserController {
         pHolderService.modify(userDto);
         //나중에 유효성 검사 및 경로 수정
         return "user/userCarList";
-
-
     }
 
 }
