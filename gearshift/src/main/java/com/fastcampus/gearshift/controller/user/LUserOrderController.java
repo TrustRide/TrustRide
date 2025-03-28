78fff3d1388dd9f2a54b03eda42c9b21bbab23b1
seoha25 <serha95@naver.com>
2025-03-28 15:03:14 +0900

    [FIX] model 매개변수 추가

--- gearshift/src/main/java/com/fastcampus/gearshift/controller/user/LUserOrderController.java
+++ gearshift/src/main/java/com/fastcampus/gearshift/controller/user/LUserOrderController.java
@@ -35,9 +35,26 @@ public class LUserOrderController {
 
     // 현금 결제 후 주문내역/배송조회 페이지 이동
     @PostMapping("/status/cash")
+    public String getOrderHistory(@ModelAttribute LOrderDTO lOrderDTO ,@ModelAttribute PaymentProcessDTO paymentProcessDTO,
+                                  @ModelAttribute LHolderDTO lHolderDTO,  @ModelAttribute DeliveryDTO deliveryDTO, Model model, HttpSession session){
 
-    public String getOrderHistory(@ModelAttribute LOrderDTO lOrderDTO , @ModelAttribute PaymentProcessDTO paymentProcessDTO,
-                                  @ModelAttribute DeliveryDTO deliveryDTO){
+
+        // userId를 session에서 꺼내주기
+        UserDto userDto =  (UserDto)session.getAttribute("loginUser");
+        Integer userId = userDto.getUserId();
+
+        // 명의자 정보 저장
+        holderService.insertHolder(lHolderDTO);
+
+        // 명의자 Id(PK) 가져오기
+        Integer holderId = lHolderDTO.getHolderId();
+
+        // dto에 userId를 넣어준다.
+        lOrderDTO.setUserId(userId);
+
+        // dto에 holderId를 넣어준다.
+        lOrderDTO.setHolderId(holderId);
+
 
         // 주문
         orderService.insertOrder(lOrderDTO);
