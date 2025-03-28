9c2f7a266fa4dd3805bc3291019356e703606760
su-jin-shin <sujinshin1@gmail.com>
2025-03-28 20:46:45 +0900

    feat: 이메일 3분 내 인증 구현, 시큐리티 적용

--- gearshift/src/main/java/com/fastcampus/gearshift/controller/user/SUserController.java
+++ gearshift/src/main/java/com/fastcampus/gearshift/controller/user/SUserController.java
@@ -8,7 +8,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 import java.util.HashMap;
 import java.util.Map;
@@ -29,9 +31,9 @@ public class SUserController {
     }
 
     @PostMapping("/register")
-    public String processRegister(@ModelAttribute UserDto user, Model model) {
+    public String processRegister(@ModelAttribute UserDto user, RedirectAttributes redirectAttributes) {
         userService.registerUser(user);
-
+        redirectAttributes.addFlashAttribute("completeMessage", "가입이 완료되었습니다!");
         return "redirect:/login.do";
     }
 
@@ -59,9 +61,9 @@ public class SUserController {
 
     @PostMapping("/send-auth-email")
     @ResponseBody
-    public Map<String, Boolean> sendEmail(@RequestBody Map<String, String> body) {
+    public Map<String, Boolean> sendEmail(@RequestBody Map<String, String> body, HttpServletRequest request) {
         String email = body.get("email");
-        boolean success = emailAuthService.sendCode(email);
+        boolean success = emailAuthService.sendCode(email, request);
         return Map.of("success", success);
     }
 
--- gearshift/src/main/java/com/fastcampus/gearshift/mail/SEmailAuthService.java
+++ gearshift/src/main/java/com/fastcampus/gearshift/mail/SEmailAuthService.java
@@ -3,11 +3,16 @@ package com.fastcampus.gearshift.mail;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
 
 import javax.mail.internet.MimeMessage;
+import javax.servlet.http.HttpServletRequest;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.time.LocalDateTime;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Random;
@@ -20,16 +25,17 @@ public class SEmailAuthService {
     private String fromEmail;
 
     private final JavaMailSender mailSender;
-    private final Map<String, String> codeStorage = new HashMap<>();
+    private final Map<String, VerificationCode> codeStorage = new HashMap<>();
 
     @Autowired
     public SEmailAuthService(JavaMailSender mailSender) {
         this.mailSender = mailSender;
     }
 
-    public boolean sendCode(String toEmail) {
+    public boolean sendCode(String toEmail, HttpServletRequest request) {
         String code = createCode();
-        codeStorage.put(toEmail, code);
+        VerificationCode verificationCode = new VerificationCode(code);
+        codeStorage.put(toEmail, verificationCode);
 
         try {
             MimeMessage message = mailSender.createMimeMessage();
@@ -38,7 +44,18 @@ public class SEmailAuthService {
             helper.setTo(toEmail);
             helper.setFrom(fromEmail);
             helper.setSubject("[Trust Ride] 이메일 인증번호입니다.");
-            helper.setText("인증번호는 " + code + " 입니다.\n3분 안에 입력해주세요.");
+
+            ClassPathResource resource = new ClassPathResource("templates/email-template.html");
+            String html = Files.readString(resource.getFile().toPath());
+
+            String imageUrl = "http://localhost:"
+                    + request.getServerPort()
+                    + request.getContextPath()
+                    + "/img/logo-orange.png";
+
+            html = html.replace("${imageUrl}", imageUrl);
+            html = html.replace("${code}", code);
+            helper.setText(html, true);
 
             mailSender.send(message);
             return true;
@@ -49,8 +66,17 @@ public class SEmailAuthService {
     }
 
     public boolean verifyCode(String email, String inputCode) {
-        String savedCode = codeStorage.get(email);
-        return savedCode != null && savedCode.equals(inputCode);
+        VerificationCode storedCode = codeStorage.get(email);
+        if (storedCode == null) return false;
+
+        // 3분 초과 여부 확인
+        LocalDateTime now = LocalDateTime.now();
+        if (now.isAfter(storedCode.getCreatedTime().plusMinutes(3))) {
+            //System.out.println("인증번호가 만료되었습니다.");
+            return false;
+        }
+
+        return storedCode.getCode().equals(inputCode);
     }
 
     private String createCode() {
